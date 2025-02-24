5c5912323dde1834e0e5acd8aa54b47174d90c6e
Iztok Elayne Bloom <5840349+bloomsei@users.noreply.github.com>
2025-02-24 19:20:05 +0100

    feat: Practice asynchronous code

--- app.ts
+++ app.ts
@@ -62,3 +62,27 @@ let Musical = class extends Video {
 let musical = new Musical('The Sound of Music', 1965);
 musical.producer = 'Robert Wise';
 musical.printCredits();
+
+function getMoviesByDirector(director: string): Promise<string[]> {
+    let p: Promise<string[]> = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const titles = GetTitles(director);
+            if (titles.length > 0) {
+                return resolve(titles);
+            }
+            reject('No movies found for that director');
+        }, 2000);
+    });
+    return p;
+}
+
+async function logSearchResults(director: string) {
+    let foundMovies = await getMoviesByDirector(director);
+    console.log(foundMovies);
+    
+}
+
+console.log('Getting movies...');
+logSearchResults('Akira Kurosawa')
+    .catch(err => console.log(err));
+console.log('Got movies!');
